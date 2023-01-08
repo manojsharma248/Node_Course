19f23c2b0f385f5789d7889cf6d34baa26c28824
manojsharma248 <35368092+manojsharma248@users.noreply.github.com>
2023-01-08 21:03:03 +0530

    add pug still in progress

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ const bodyParser = require("body-parser");
 const rootDir = require("./utils/path");
 
 const app = express();
+
+app.set("view engine", "pug");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes.router);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).render("404");
 });
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,14 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 const rootDir = require("../utils/path");
+
+const products = [];
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { pageTitle: "Add Product" });
 });
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+module.exports = { router, products };
