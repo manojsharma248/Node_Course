6a4eda2aac62712f818bdf101366998776c91047
manojsharma248 <35368092+manojsharma248@users.noreply.github.com>
2023-12-10 19:07:01 +0530

    Linking to edit page

--- models/cart.js
+++ models/cart.js
@@ -1,38 +1,40 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "cart.json"
+  'data',
+  'cart.json'
 );
+
 module.exports = class Cart {
   static addProduct(id, productPrice) {
+    // Fetch the previous cart
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
       if (!err) {
         cart = JSON.parse(fileContent);
       }
-      //Analyze the cars => Find the existing product
-      let existingProductIndex = cart.products.findIndex(
-        (product) => product.id === id
+      // Analyze the cart => Find existing product
+      const existingProductIndex = cart.products.findIndex(
+        prod => prod.id === id
       );
-      let existingProduct = cart.products[existingProductIndex];
+      const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
-      //Add new product/increse quantity
+      // Add new product/ increase quantity
       if (existingProduct) {
         updatedProduct = { ...existingProduct };
-        updatedProduct.qyt = updatedProduct.qyt + 1;
+        updatedProduct.qty = updatedProduct.qty + 1;
         cart.products = [...cart.products];
         cart.products[existingProductIndex] = updatedProduct;
       } else {
-        updatedProduct = { id: id, qyt: 1 };
+        updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
       }
       cart.totalPrice = cart.totalPrice + +productPrice;
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
+      fs.writeFile(p, JSON.stringify(cart), err => {
         console.log(err);
       });
     });
-    //fetch the prev card
   }
 };
