3057bd7462b1afe0ca06b6c57df698517f7ca48c
manojsharma248 <35368092+manojsharma248@users.noreply.github.com>
2023-12-15 13:40:16 +0530

    Creating Product

--- util/database.js
+++ util/database.js
@@ -1,17 +1,26 @@
 const mongodb = require("mongodb");
-const MongoClient = mongodb.mongodbClient;
-
+const MongoClient = mongodb.MongoClient;
+let _db;
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    `mongodb+srv://Mani:S9FHYgrYHQma7pRh@cluster0.7mzkqc4.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://Mani:S9FHYgrYHQma7pRh@cluster0.7mzkqc4.mongodb.net/shop?retryWrites=true&w=majority`
   )
     .then((client) => {
       console.log("Connection established");
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch((err) => {
       console.log("Error", err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
