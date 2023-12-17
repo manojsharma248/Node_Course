61c458fd171b5933ab1562ecc67bddcbd9b30e6c
manojsharma248 <35368092+manojsharma248@users.noreply.github.com>
2023-12-17 12:48:58 +0530

    Creating A New User

--- models/user.js
+++ models/user.js
@@ -1,16 +1,28 @@
-const Sequelize = require('sequelize');
-
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
+const ObjectId = mongodb.ObjectId;
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+  }
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .then((user) => {
+        console.log("in user", user);
+        return user;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+}
 
 module.exports = User;
