1cbaeaddaa48c9987321d40a9bedc578339d4b15
manojsharma248 <35368092+manojsharma248@users.noreply.github.com>
2023-12-18 11:09:23 +0530

    Cart Implmentation

--- models/user.js
+++ models/user.js
@@ -2,13 +2,28 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 const ObjectId = mongodb.ObjectId;
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.username = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex((item) => {
+    //   return item.id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("products")
+      .updatedOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
   static findById(userId) {
     const db = getDb();
