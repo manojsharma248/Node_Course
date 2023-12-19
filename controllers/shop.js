2775b0d3d5fcd4e95e4619f7ad065c51acc5f973
manojsharma248 <35368092+manojsharma248@users.noreply.github.com>
2023-12-19 15:26:49 +0530

    Displaying the Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -54,17 +54,12 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then((cart) => {
-      return cart
-        .getProducts()
-        .then((products) => {
-          res.render("shop/cart", {
-            path: "/cart",
-            pageTitle: "Your Cart",
-            products: products,
-          });
-        })
-        .catch((err) => console.log(err));
+    .then((products) => {
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: products,
+      });
     })
     .catch((err) => console.log(err));
 };
@@ -75,8 +70,8 @@ exports.postCart = (req, res, next) => {
     .then((product) => {
       return req.user.addToCart(product);
     })
-    .then((result) => {
-      console.log("Result ***", result);
+    .then(() => {
+      res.redirect("/cart");
     });
   // let fetchedCart;
   // let newQuantity = 1;
--- models/user.js
+++ models/user.js
@@ -38,6 +38,24 @@ class User {
         { $set: { cart: updatedCart } }
       );
   }
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((item) => item.productId);
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((item) => {
+              return item.productId.toString() === p._id.toString();
+            }).quantity,
+          };
+        });
+      });
+  }
   static findById(userId) {
     const db = getDb();
     return db
