f63d4527627272e99295591d31cf720a19d6f24c
manojsharma248 <35368092+manojsharma248@users.noreply.github.com>
2023-12-26 19:40:15 +0530

    257. adding sign in functionalty

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,14 +18,32 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("5bab316ce0a7c75f783cb8a8")
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log("postLogin", email, password);
+  User.findOne({ email: email })
     .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect("/");
-      });
+      console.log("user", user);
+      if (!user) {
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              return res.redirect("/");
+            });
+          }
+          return res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          return res.redirect("/login");
+        });
     })
     .catch((err) => console.log(err));
 };
