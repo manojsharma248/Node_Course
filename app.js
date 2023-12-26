40c82ba573a0a3a99bb07b3937b6e6189c037e3c
manojsharma248 <35368092+manojsharma248@users.noreply.github.com>
2023-12-26 19:05:56 +0530

    254. Implementing an Authentication Flow

--- app.js
+++ app.js
@@ -57,18 +57,18 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Mani",
-          email: "mani@gmail.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
+    // User.findOne().then((user) => {
+    //   if (!user) {
+    //     const user = new User({
+    //       name: "Mani",
+    //       email: "mani@gmail.com",
+    //       cart: {
+    //         items: [],
+    //       },
+    //     });
+    //     user.save();
+    //   }
+    // });
 
     app.listen(3000);
   })
