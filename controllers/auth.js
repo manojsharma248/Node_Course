651cf537ba7a66bee7cff94e772db57826757ee1
manojsharma248 <35368092+manojsharma248@users.noreply.github.com>
2023-12-26 18:46:39 +0530

    253. Our Project updated status

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,39 @@
-const User = require("../models/user");
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  // console.log(req.get("Cookie"));
-  // const isLoggedIn = req.get("Cookie").trim().split("=")[1] === "true";
-  console.log(req.session);
-  res.render("auth/login", {
-    path: "/login",
-    pageTitle: "Login",
-    isAuthticated: false,
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("6583d41329848f4b08c8d389")
-    .then((user) => {
-      req.session.user = user;
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
       req.session.isLoggedIn = true;
-      req.session.save((err) => {
-        console.error(err);
-        res.redirect("/");
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
       });
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
-exports.postLogout = (req, res) => {
-  req.session.destroy(() => {
-    res.redirect("/");
+exports.postSignup = (req, res, next) => {};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
   });
 };
