0d0873a5f919bf571ca47bcab603ce85d9eab8b6
manojsharma248 <35368092+manojsharma248@users.noreply.github.com>
2023-12-30 15:42:27 +0530

    372. Understanding the Frontend Setup

--- routes/feed.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const feedConroller = require("../controller/feed");
-
-router.get("/posts", feedConroller.getPosts);
-router.post("/post", feedConroller.createPost);
-
-module.exports = router;
